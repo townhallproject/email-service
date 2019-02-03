d03b8ea2cf48549d86b54d01c5d0433d07d91b92
meganrm <meganrm@gmail.com>
2019-02-02 19:22:05 -0800

    seperated out get events call

---
+++ bin/get-events.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const getTownHalls = require('../townhall/getTownHalls');
+const TownHall = require('../townhall/townhall-model');
+console.log('NODE_ENV:', process.env.NODE_ENV);
+
+getTownHalls().then(function(){
+  console.log('got events');
+  console.log(TownHall.stateEvents);
+});
